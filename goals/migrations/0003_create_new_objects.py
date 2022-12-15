e8222b06750ef02df7f9859d6fc0ae239769b460
Kapitolina <Kapitolina999@gmail.com>
2022-12-16 02:12:45 +0500

    board

--- goals/migrations/0003_create_new_objects.py
+++ goals/migrations/0003_create_new_objects.py
@@ -5,9 +5,9 @@ from django.utils import timezone
 
 def create_objects(apps, schema_editor):
     User = apps.get_model('core', 'User')
-    Board = apps.get_model('goals.models', 'Board')
-    BoardParticipant = apps.get_model('goals.models', 'BoardParticipant')
-    GoalCategory = apps.get_model('goals.models', 'GoalCategory')
+    Board = apps.get_model('goals', 'Board')
+    BoardParticipant = apps.get_model('goals', 'BoardParticipant')
+    GoalCategory = apps.get_model('goals', 'GoalCategory')
 
     with transaction.atomic():
         for user in User.objects.all():
