3e8fda2a7419f115b70d72ede048a96b52ac0912
Kapitolina <Kapitolina999@gmail.com>
2022-12-27 02:18:17 +0500

    38 migrate

--- bot/tg/client.py
+++ bot/tg/client.py
@@ -11,7 +11,7 @@ class TgClient:
         return f'https://api.telegram.org/bot{self.token}/{method}'
 
     def get_updates(self, offset: int = 0, timeout: int = 60) -> dc.GetUpdatesResponse:
-        url = self.get_url('getUpdates')
+        # url = self.get_url('getUpdates')
         # response = requests.get(url, params={'offset': offset, 'timeout': timeout,
         #                                      'allowed_updates': "['update_id', 'message']"})
         response = requests.get(self.get_url(f'getUpdates?offset={offset}&timeout={timeout}&'
